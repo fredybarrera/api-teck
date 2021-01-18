f1736e9bd32399273251671e309ce443f2f984b4
Fredy Barrera Artiaga <49963170+fredybarrera@users.noreply.github.com>
2021-01-18 10:32:05 -0300

    Ajustes deploy IIS

--- api_teck/urls.py
+++ api_teck/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     url(r'^api/', include('equip.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
